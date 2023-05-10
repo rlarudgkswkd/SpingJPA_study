e9bf2072d900d461ea593869cf9955ce6e936c81
rlarudgkswkd <rlarudgkswkd@gmail.com>
2023-05-10 22:59:47 +0900

    lecture 2

--- springJpaStudy/src/main/java/org/example/Main.java
+++ springJpaStudy/src/main/java/org/example/Main.java
@@ -15,29 +15,22 @@ public class Main {
         tx.begin();
 
         try{
-            //INSERT
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("hellob");
-//              em.persist(member);
 
-            //Find
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember.id : " + findMember.getId());
-//            System.out.println("findMember.name : " + findMember.getName());
+            //비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("helloJpa");
 
-            //Modify
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJpa");
+            //여기서 부터 영속 상태, 엔티티 매니저라는 곳에 영속성 컨텍스트에서 관리됌. 근데 아직 DB에 저장 안됌.
+            System.out.println("====Before====");
+            em.persist(member);
+            System.out.println("====After====");
 
-            List<Member> findMembers = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
+            //준영속, 영속에서 지움.
+            em.detach(member);
 
-            for (Member findMember : findMembers) {
-                System.out.println("member.name = " + findMember.getName());
-            }
+            //삭제, 실제 db에서 지우겠다.
+//            em.remove(member);
 
             tx.commit();
         }catch (Exception e){
