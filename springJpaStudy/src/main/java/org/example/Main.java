ef3dee723ebbe8f53dc6a870ed51c81bdc4f5e22
rlarudgkswkd <rlarudgkswkd@gmail.com>
2023-05-11 09:39:59 +0900

    영속성 컨텍스트 이점 : 영속 엔티티의 동일성 보장1

--- springJpaStudy/src/main/java/org/example/Main.java
+++ springJpaStudy/src/main/java/org/example/Main.java
@@ -22,8 +22,8 @@ public class Main {
             //똑같은거 조회할때는 select 쿼리가 안돌고 1차 캐시에서 가져옴을 볼수있음.
             Member findMember2 = em.find(Member.class,101L);
 
-            //삭제, 실제 db에서 지우겠다.
-//            em.remove(member);
+            //영속 엔티티의 동일성 보장
+            System.out.println("result = " + (findMember1 == findMember2));
 
             tx.commit();
         }catch (Exception e){
